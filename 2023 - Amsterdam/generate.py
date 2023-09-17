2103bcf7518d644787583e016fbc614359716cb2
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-09-17 09:29:32 +0200

    Add room numbers

--- 2023 - Amsterdam/generate.py
+++ 2023 - Amsterdam/generate.py
@@ -4,15 +4,15 @@ import requests
 schedule = requests.get('https://amsterdam2023.pydata.org/cfp/schedule/export/schedule.json').json()
 
 # for each talk, generate a markdown file
-# format: "Day N/room name/start time - title/README.md"
+# format: "Day N/room number - room name/start time - title/README.md"
 for i, day in enumerate(schedule['schedule']['conference']['days']):
     day_path = Path(f"Day {i+1}")
     day_path.mkdir(exist_ok=True)
-    for talks in day['rooms'].values():
+    for roomN, talks in enumerate(day['rooms'].values()):
         for talk in talks:
-            # escape special characters
+            # forward slash not allowed, replace with unicode forward slash
             talk_title = talk['title'].replace('/', '∕')
-            talk_path = day_path / talk["room"] / f"{talk['start']} - {talk_title}"
+            talk_path = day_path / f"{roomN + 1} - {talk['room']}" / f"{talk['start']} - {talk_title}"
             talk_path.mkdir(exist_ok=True, parents=True)
             all_speakers = ", ".join([p['public_name'] for p in talk['persons']])
             with open(talk_path / 'README.md', 'w') as f:
